b98420f244c0dede74ab4bfdc33ef750fa235bf2
oephi <r.sickle@protonmail.com>
2019-01-17 12:11:36 +1100

    added .gitignore file

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 const express = require("express");
-const app = express();
+const session = require("express-session");
+const cors = require("cors");
 const routes = require("./routes");
+
+const app = express();
+
+app.use(session({
+  secret:
+}))
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded());
